6d23013251e677f704f2a41a696648b88c573679
Andrew Stone <andy@stonean.com>
2008-11-15 14:53:02 -0500

    release of 0.6.0

--- lib/lockdown/classy-inheritance.rb
+++ lib/lockdown/classy-inheritance.rb
@@ -1,12 +1,13 @@
-unless Object.const_defined?("Stonean") && Stonean.const_defined?("ClassyInheritance")
-  begin
-    require "classy-inheritance"
-  rescue LoadError
-    puts <<-MSG 
-      You need to install classy-inheritance to use the provided models.
-      With gems, use `gem install classy-inheritance'
-    MSG
-    exit
+if Object.const_defined?("ActiveRecord")
+  unless  Object.const_defined?("Stonean") && Stonean.const_defined?("ClassyInheritance")
+    begin
+      require "classy-inheritance"
+    rescue LoadError
+      puts <<-MSG 
+        You need to install classy-inheritance to use the provided models.
+        With gems, use `gem install classy-inheritance'
+      MSG
+      exit
+    end
   end
-end
-             
+end           
