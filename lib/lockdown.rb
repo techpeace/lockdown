b9c316af93b6d8f37fa72b9d4da0ce33ca4bb44f
Andrew Stone <andy@stonean.com>
2008-12-27 15:38:45 -0500

    adding history file. modified lockdown.rb to play nice outside of framework use

--- lib/lockdown.rb
+++ lib/lockdown.rb
@@ -2,33 +2,49 @@ require File.join(File.dirname(__FILE__), "lockdown", "classy-inheritance")
 require File.join(File.dirname(__FILE__), "lockdown", "helper")
 
 module Lockdown
+
+  # :stopdoc:
+  VERSION = '0.7.0'
+  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
+  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
+  # :startdoc:
+  
   class << self
     include Lockdown::Helper
 
+    # Returns the version string for the library.
+    #
+    def version
+      VERSION
+    end
+ 
+    # Mixin Lockdown code to the appropriate Controller and ORM
+    #
     def mixin
       if mixin_resource?("frameworks")
         unless mixin_resource?("orms")
           raise NotImplementedError, "ORM unknown to Lockdown!"
         end
       else
-        raise NotImplementedError, "Framework unknown to Lockdown!"
+        puts "Environment unknown to Lockdown! Assuming I was required for a rake task or similar utility."
       end
-    end
+    end # mixin
 
     private
 
     def mixin_resource?(str)
-      Dir["#{File.dirname(__FILE__)}/lockdown/#{str}/*.rb"].each do |f|
-        require "#{f}"
-        mod = File.basename(f).split(".")[0]
-        mklass = eval("Lockdown::#{str.capitalize}::#{Lockdown.camelize(mod)}")
-        if mklass.use_me?
-          include mklass
+      wildcard_path = File.join( File.dirname(__FILE__), %w['lockdown', str, '*.rb'] ) 
+      Dir[wildcard_path].each do |f|
+        require f
+        module_name = File.basename(f).split(".")[0]
+        module_class = eval("Lockdown::#{str.capitalize}::#{Lockdown.camelize(module_name)}")
+        if module_class.use_me?
+          include module_class
           return true
         end
       end
       false
-    end
+    end # mixin_resource?
   end # class block
 end # Lockdown
 
