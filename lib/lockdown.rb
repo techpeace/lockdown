4631cbf9f73de57e1e8f7d438fe69705d1a9fb8a
Andrew Stone <andy@stonean.com>
2009-01-17 20:04:23 -0500

    updated version number for release 0.7.1

--- lib/lockdown.rb
+++ lib/lockdown.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), "lockdown", "helper")
 
 module Lockdown
   # :stopdoc:
-  VERSION = '0.7.0'
+  VERSION = '0.7.1'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
