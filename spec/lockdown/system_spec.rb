0a6be716a6ba68a9a06b7f626f13e8db4977fe83
Andrew Stone <andy@stonean.com>
2009-01-29 21:52:38 -0500

    removed rights related ... and stupid ... specs

--- spec/lockdown/system_spec.rb
+++ spec/lockdown/system_spec.rb
@@ -2,18 +2,6 @@ require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 require File.join(File.dirname(__FILE__), %w[.. .. lib lockdown rules])
 
 describe Lockdown::System do
-  it "should require rights if version <= 0.7" do
-    Lockdown.stub!(:version).and_return('0.7.1')
-    Lockdown::Rights.should_receive(:extended).with(Lockdown::System)
-    load File.join(File.dirname(__FILE__), %w[.. .. lib lockdown system.rb])
-  end
-
-  it "should require rules if version > 0.7" do
-    Lockdown.stub!(:version).and_return('0.8.0')
-    Lockdown::Rules.should_receive(:extended).with(Lockdown::System)
-    require File.join(File.dirname(__FILE__), %w[.. .. lib lockdown system.rb])
-  end
-
   it "should fetch the option" do
     Lockdown::System.options = {}
     Lockdown::System.options['test'] = "my test"
@@ -26,5 +14,27 @@ describe Lockdown::System do
     Lockdown::System.controller_classes = {}
     Lockdown::System.controller_classes[:users] = klass
     Lockdown::System.fetch_controller_class(:users).should equal(klass)
+  end
+
+  describe "#paths_for" do
+    it "should join the str_sym to the methods" do 
+      Lockdown::System.paths_for(:users, :show, :edit).
+        should == ["users/show", "users/edit"]
+    end
+
+    it "should add users to the array if access is granted on index" do 
+      Lockdown::System.paths_for(:users, :index, :show, :edit).
+        should == ["users/index", "users/show", "users/edit", "users"]
+    end
+
+    it "should build the paths from the controller class if no methods specified" do
+      methods = ["new","edit","create","update"]
+      Lockdown::System.stub!(:fetch_controller_class)
+      Lockdown::System.stub!(:available_actions).
+        and_return(methods)
+
+      Lockdown::System.paths_for(:users).
+        should == ["users/new","users/edit","users/create","users/update"]
+    end
   end
 end
