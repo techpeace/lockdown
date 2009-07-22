7087ce2fc87e1d37f77e7b7f7ea9f45ca630de96
Andrew Stone <andy@stonean.com>
2009-07-22 19:45:02 -0400

    updating to version 1.0.4

--- lib/lockdown.rb
+++ lib/lockdown.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), "lockdown", "helper")
 module Lockdown
   extend Lockdown::Helper
 
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 
   # Returns the version string for the library.
   def self.version
